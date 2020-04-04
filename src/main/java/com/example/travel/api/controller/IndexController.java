1208b42d22ebb822c3aad0fa31b84b7bda1be8f5
Qusijia <2529886960@qq.com>
2020-04-04 21:45:59 +0800

    修改登录注册界面，修改主页面

--- src/main/java/com/example/travel/api/controller/IndexController.java
+++ src/main/java/com/example/travel/api/controller/IndexController.java
@@ -4,9 +4,7 @@ package com.example.travel.api.controller;
 import com.example.travel.entity.User;
 import com.example.travel.service.UserService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,51 +18,41 @@ public class IndexController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/admin")
-    public ModelAndView mShift(HttpSession session) {
-        ModelAndView result = new ModelAndView();
-        result.setViewName("admin_manage");//转入后台管理页面
-        result.addObject("name",session.getAttribute("name"));
-        return result;
-    }
+//    @RequestMapping("/admin")
+//    public ModelAndView mShift(HttpSession session) {
+//        ModelAndView result = new ModelAndView();
+//        result.setViewName("admin_manage");//转入后台管理页面
+//        result.addObject("name",session.getAttribute("name"));
+//        return result;
+//    }
+//
 
+    @RequestMapping("/test")
+    public String test() {
+        System.out.println("visitor visitor visitor visitor");
+        return "1111111111========333333333333333";
+    }
     @RequestMapping("/visitor")
     public ModelAndView sShift() {
+        System.out.println("visitor visitor visitor visitor");
         ModelAndView result = new ModelAndView();
-        result.setViewName("index");//转入游客界面
+        result.setViewName("test");//转入游客界面
         return result;
     }
 
-//    @GetMapping("/mlogin")
-//    public String mLogin(HttpSession session, HttpServletRequest request) {
-//        String username = request.getParameter("username");//获取前台界面的username数据
-//        String password = request.getParameter("password");//获取前台界面的password数据
-//
-//        User user = userService.get(username, password);//调用userservice进行数据库查询
-//        if (user != null) {//用户存在
-//            session.setAttribute("user", user);//将角色设置到session
-//            if (user.getRole() == 0) {
-//                session.setAttribute("id", user.getId());//教务：将id存入session
-//                System.out.println("11111111-------"+session.getAttribute("user"));
-//            } else {
-//                return "2";
-//            }
-//            return "1";//用户存在返回1
-//        } else {
-//            return "0";//用户不存在返回0
-//        }
-//    }
 
     @RequestMapping("/mindex")
     public ModelAndView mIndex() {
         ModelAndView result = new ModelAndView();
-        result.setViewName("login");
+        result.setViewName("index");
         return result;
     }
 
 
-    @GetMapping("/mlogin")
-    public String login(String username, String password, HttpSession session){
+    @RequestMapping("/mlogin")
+    public ModelAndView login(String username, String password, HttpSession session){
+        System.out.println("11111111111111"+username);
+        ModelAndView modelAndView = new ModelAndView();
         System.out.println("name="+username);
         /**
          * 使用Shiro编写认证操作
@@ -76,31 +64,44 @@ public class IndexController {
         UsernamePasswordToken token = new UsernamePasswordToken(username,password);
 
         //3.执行登录方法
+
         try {
             subject.login(token);
-
-           session.setAttribute("name",username);
-            return "1";
-        } catch (UnknownAccountException e) {
-            //e.printStackTrace();
-            //登录失败:用户名不存在
-            return "2";
-        }catch (IncorrectCredentialsException e) {
-            //e.printStackTrace();
-            //登录失败:密码错误
-            return "3";
+        } catch (UnknownAccountException uae) {
+            modelAndView.addObject("tips","*未知账户~");
+            modelAndView.setViewName("index");
+            return modelAndView;
+        } catch (IncorrectCredentialsException ice) {
+            modelAndView.addObject("tips","*密码不正确~");
+            modelAndView.setViewName("index");
+            return modelAndView;
+        }catch (ExcessiveAttemptsException eae) {
+            modelAndView.addObject("tips","*用户名或密码错误次数过多~");
+            modelAndView.setViewName("index");
+            return modelAndView;
+        } catch (AuthenticationException ae) {
+            modelAndView.addObject("tips","*用户名或密码不正确~");
+            modelAndView.setViewName("index");
+            return modelAndView;
         }
+        modelAndView.setViewName("main");
+        modelAndView.addObject("name",username);
+        return modelAndView;
+
+
     }
 
     @RequestMapping("/register")
     public ModelAndView register() {
         ModelAndView result = new ModelAndView();
-        result.setViewName("register");//转入注册界面
+        result.setViewName("regist");//转入注册界面
         return result;
     }
 
-    @PostMapping("/yanzheng")
+    @RequestMapping("/yanzheng")
     public String yanzheng(@RequestBody User u) {
+
+        System.out.println("555555555555555=====   ");
         String username = u.getName();//获取前台界面的username数据
         u.setLim(0);
         System.out.print("11111111   " +u);
